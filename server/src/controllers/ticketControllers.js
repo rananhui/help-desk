9ec3339bcb5f1fa8d93b2e57d64fbd9e4ec18a1e
Ranan Hui <rananhui@Ranans-MacBook-Air.local>
2023-08-15 16:09:12 -0400

    added update status controller

--- server/src/controllers/ticketControllers.js
+++ server/src/controllers/ticketControllers.js
@@ -19,29 +19,53 @@ ticketController.generateTicketId = async (req, res, next) => {
 ticketController.submitTicket = (req, res, next) => {
     const { name, email, subject, description } = req.body
     const { ticketId } = res.locals.ticketId
-    Ticket.create({name, email, subject, description, ticketId}, (err, data) => {
-        if(err){
-            return next({
-                log: 'Express error occurred submitTicket: ' + err,
-                status: 400,
-                message: { err: 'ticketController.submitTicket: An error occurred' },
-            })
-        }
-        return next();
+    // Ticket.create({name, email, subject, description, ticketId}, (err, data) => {
+    //     if(err){
+    //         return next({
+    //             log: 'Express error occurred submitTicket: ' + err,
+    //             status: 400,
+    //             message: { err: 'ticketController.submitTicket: An error occurred' },
+    //         })
+    //     }
+    //     return next();
+    // })
+    Ticket.create({name, email, subject, description, ticketId})
+    .then((data) => {
+        console.log(data)
+        return next()
+    })
+    .catch((err) => {
+        return next({
+            log: 'Express error occurred submitTicket: ' + err,
+            status: 400,
+            message: { err: 'ticketController.submitTicket: An error occurred' },
+        })
     })
 }
 
 ticketController.getTickets = (req, res, next) => {
-    Ticket.find({}, (err, tickets) => {
-        if(err){
-            return next({
-                log: 'Express error occurred getTicket: ' + err,
-                status: 400,
-                message: { err: 'ticketController.getTickets: An error occurred' },
-            })
-        }
+    // Ticket.find({}, (err, tickets) => {
+    //     if(err){
+    //         return next({
+    //             log: 'Express error occurred getTicket: ' + err,
+    //             status: 400,
+    //             message: { err: 'ticketController.getTickets: An error occurred' },
+    //         })
+    //     }
+    //     res.locals.tickets = tickets
+    //     return next();
+    // })
+    Ticket.find({})
+    .then((tickets) => {
         res.locals.tickets = tickets
-        return next();
+        return next()
+    })
+    .catch((err) => {
+        return next({
+            log: 'Express error occurred getTicket: ' + err,
+            status: 400,
+            message: { err: 'ticketController.getTicket: An error occurred' },
+        })
     })
 }
 
@@ -51,6 +75,22 @@ ticketController.respondToTicket = (req, res, next) => {
     return next()
 }
 
+ticketController.updateStatus = (req, res, next) => {
+    const { ticketId } = req.body.ticketId
+    const { newStatus } = req.body.status
+    Ticket.updateOne({ticketId: ticketId}, {$set: {status: newStatus}}, {new: true})
+    .then((data) => {
+        console.log(data)
+        return next()
+    })
+    .catch((err) => {
+        return next({
+            log: 'Express error occurred updateStatus: ' + err,
+            status: 400,
+            message: { err: 'ticketController.updateStatus: An error occurred' },
+        })
+    })
+}
 
 
 
