c453a2a2baaedf24146366bc90361d24c0688050
Ranan Hui <rananhui@Ranans-MacBook-Air.local>
2023-08-16 00:15:50 -0400

    edited ticket page

--- client/src/components/TicketPage.jsx
+++ client/src/components/TicketPage.jsx
@@ -10,13 +10,33 @@ const TicketPage = () => {
     })
 
     const handleChange = (e) => {
-        const {type, value} = e.target
-        setTicket(prevValues => ({...prevValues, [type]: value}))
+        const {name, value} = e.target
+        setTicket(prevValues => ({...prevValues, [name]: value}))
     }
     
     const handleSubmit = (e) => {
         e.preventDefault()
-
+        fetch('https://help-desk-w8aq.onrender.com/ticket', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(ticket)
+        })
+        .then((response) => {
+            if(response.ok){
+                console.log('Ticket submitted successfully')
+                setTicket({
+                    name: '',
+                    email: '',
+                    subject: '',
+                    description: ''
+                })
+            }else {
+                console.log('Ticket submission failed')
+            }
+        })
+        .catch((err) => { console.log('An error has occurred: ', err)})
     }
     return (
         <div className="ticket-container">
@@ -25,7 +45,7 @@ const TicketPage = () => {
                 <input type="text" name="name" placeholder="Name" value={ticket.name} onChange={handleChange}/>
                 <input type="text" name="email" placeholder="Email" value={ticket.email} onChange={handleChange}/>
                 <input type="text" name="subject" placeholder="Subject" value={ticket.subject} onChange={handleChange}/>
-                <textarea name="description" placeholder="Please enter a detailed description of your issue" value={ticket.name} onChange={handleChange}/>
+                <textarea name="description" placeholder="Please enter a detailed description of your issue" value={ticket.description} onChange={handleChange}/>
                 <button type="submit">Submit ticket</button>
             </form>
         </div>
